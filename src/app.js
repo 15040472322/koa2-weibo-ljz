a4de89cb1fa435c62584bdfaaaacb9f47becea16
15040472322 <973825343@qq.com>
2020-06-04 15:53:41 +0800

    feat session

--- src/app.js
+++ src/app.js
@@ -5,9 +5,12 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const { REDIS_CONF } = require('./conf/db')
 
 // error handler
 onerror(app)
@@ -24,6 +27,21 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
+//session 配置
+app.keys = ['UIsd_13']
+app.use(session({
+  key: 'weibo.sid',       // cookie name  默认是 `koa.sid`
+  prefix: 'weibo:sess:',  // redis key的前缀，默认是  `koa:sess:`
+  cookie: {
+    path: '/',            //在整个网络所有目录都能访问
+    httpOnly: true,       //只能通过server端改，不能通过客户端修改
+    maxAge: 24 * 60 * 60 * 1000  //过期时间
+  },
+  //ttl: 24 * 60 * 60 * 7 * 1000,  cookie写好过期时间默认redis过期时间和上面一致
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
+}))
 // // logger
 // app.use(async (ctx, next) => {
 //   const start = new Date()
